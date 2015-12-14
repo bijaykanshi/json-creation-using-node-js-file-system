0eaa0102d506179d19cd784962fbddeb7c8fd9ce
Bijay Kanshi <bijay.iit12@gmail.com>
2015-12-14 19:12:42 +0530

    sf

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-	app = express();
-
-var port = process.env.PORT || 8087;
-require('./config')(app);
-require('./routes')(app);
-app.listen(port);
-console.log('Your application is running on http://localhost:' + port);
-//app.listen(port);
